e6d7c12f913847b29b79d13c1a9c10b84a351901
Philip Bale <pbale95@gmail.com>
2015-07-30 21:46:05 -0700

    Initial commit

---
+++ fakephonecall/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  fakephonecall
+//
+//  Created by Philip Bale on 7/30/15.
+//  Copyright (c) 2015 Philip Bale. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
